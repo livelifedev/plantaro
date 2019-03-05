1dbecc89557c63e55722a7ae2ad883b76ee0277f
Juanma <juanmasanchezortiz@hotmail.com>
2019-03-05 22:56:43 +1100

    merged codes and water/sun bug solved

--- plantaro.rb
+++ plantaro.rb
@@ -6,77 +6,171 @@ require 'rainbow/refinement'
 using Rainbow
 require "tty-font"
 font = TTY::Font.new(:doom)
-require 'catpix'
+# require 'catpix'
 
 
 class Plant
-  attr_accessor :birthday, :age
+  attr_accessor :birthday, :age #just for testing
   
-  # messages = {:statuts => ["Your plant is vigorous!", "Your plant is OK", "Your plant is feeling standard...", "Mate! Your plant is sad and weak!", "Your plant died! Rest In Plant... Good plant, better person"], :command => {bad_decidion => ["Too much water! This is not the Titanic!", "Too much sun! Your plant became a tomato", "Too much pestizide! Achoo!", "Don´t shout!", "Insects!"], good decision => "Good decision!"}}
-#good decision --> messages[2][2]; bad decisions --> messages[2][1][...]; status --> messages[1][...]
   def initialize(username, name, birthday = Date.new(1993, 04, 16))
     @owner = username
     @name = name
-    @happiness = 0
+    @happiness = 20
     @birthday = birthday
     @age = (Date.today - @birthday).to_i
     @pest = false
+    @messages = {
+      :happy100 => "#{@name} is vigorous! Like a beanstalk", 
+      :happy75 => "#{@name}'s stem is looking thick and muscular", 
+      :happy50 => "#{@name} is chillin like a plant",
+      :happy30 => "#{@name} is feeling standard...",  
+      :happy20 => "Mate! #{@name} is sad and weak!",
+      :dead => "#{@name} died! Rest In Plant... Good plant, better person", 
+      :water => "#{@name} is glistening! sparkle sparkle.",
+      :sun => "#{@name} is getting a nice healthy tan.",
+      :song => "#{@name} is reacting to your angel-like voice.",
+      :healed => "You saved #{@name} from ferocious pests, Hero!!",
+      :overwater => "Too much water! #{@name} is not the Titanic!", 
+      :oversun => "Too much sun! #{@name} became a tomato", 
+      :poison => "Too much pesticide! #{@name} looks diseased!", 
+      :noisy => "Don´t shout. #{@name} is scared!"
+    }
     @last_action = []
   end
 
-  def  water_plant #I want to water the plant. If I had watered it early, it´s less happy. If not, it´s happier.
-    @last_action.push "water"
-    if @last_action[-1] == @last_action[-2]
-      @happiness -= 10
-      #then here we must display a sad message
-    else
-      @happiness += 10
-      puts @happiness
-      #then here we must display a good message
+  def check_happiness
+
+    info = "Your plant's name is #{@name}, " +
+    "this fella was born on #{@birthday}, " +
+    "thanks to you #{@owner}:)"
+
+    puts "#{info} \nPlant status:"
+
+    if @happiness >= 100
+      puts @messages[:happy100]
+    elsif @happiness >= 75
+      puts @messages[:happy75]
+    elsif @happiness >= 50
+      puts @messages[:happy50]
+    elsif @happiness >= 30
+      puts @messages[:happy30]
+    elsif @happiness < 30
+      puts @messages[:happy20]
     end
   end
 
-  def give_sun
-  @last_action.push "sun"
-    if @last_action[-1] == @last_action[-2]
+  def water
+    if @last_action[-1] == @last_action[-2] && @last_action[-1] == "water" #needs 3 to be too much
+      # ((@last_action[-2] == @last_action[-3] || @last_action[-1] == @last_action[-2]) && @last_action.size > 1) #when there have been carried out more than two actions, what takes into account the program is the previous two actions
       @happiness -= 10
-      #then here we must display a sad message
+      puts @messages[:overwater]
     else
       @happiness += 10
-      #then here we must display a good message
+      puts @messages[:water]
     end
+    @last_action.push "water"
+  end
+
+  def give_sun
+      if @last_action[-1] == @last_action[-2] && @last_action[-1] == "give sun" #needs 2 to be two much
+        # ((@last_action[-2] == @last_action[-3] || @last_action[-1] == @last_action[-2]) && @last_action.size > 1)
+        @happiness -= 10
+        puts @messages[:oversun]
+      else
+        @happiness += 10
+        puts @messages[:sun]
+      end
+    @last_action.push "give sun"
   end
 
   def sing
     puts "What do you want to sing to your plant?"
     song = gets.chomp
-    if song == song.upcase #in case the owner shouts at the plant
+    #shouting in upcase will reduce happiness
+    if song == song.upcase 
       @happiness -= 10
-      #then here we must display a sad message
+      puts @messages[:noisy]
     else
-      @happiness += 10
-      #then here we must display a good message
+      #length of song will add bonus points to happiness
+      @happiness += 10 + song.length
+      puts @messages[:song]
     end
+    @last_action.push "sing"
   end
 
-  # def insects
-  #   if @pests == true
-  #     @happiness -= 10
-  #   else
-
-      
-
-  def pestizide
-    @last_action.push "pestizide"
-    if @last_action[-1] == @last_action[-2]
-      @happiness -= 10
-      #then here we must display a sad message
-    else
-      @pests = false
-      #then here we must display a good message
+  def pest?
+    #33% chance to get infested with bugs, previous action must not be spray
+    #will not run if no other action has been taken first (newly created)
+    if rand(300) == 1 && @last_action[-1] != "spray" && @last_action != []
+      @pest = true
+      @happiness -= 20
     end
+    @pest
   end
 
+  def spray
+    if @pest
+      #spraying plant while infested will return lost points
+      @happiness += 20
+      @pest = false
+      puts @messages[:healed]
+    else
+      #if plant is not infested with bugs, spray will reduce happiness
+      @happiness -= 10
+      puts @messages[:poison]
+    end
+    @last_action.push "spray"
+  end
+
+  def death?
+    @happiness <= 0
+  end
+end
+
+
+puts "Give your little plantling a name?"
+plant_name = gets.chomp
+plantaro = Plant.new "John", plant_name
+
+#puts plantaro.birthday
+#puts plantaro.age
+
+#program loop
+loop do 
+  if plantaro.death?
+    puts "Your plant has withered away dues to poor plant-caring skills :("
+    break
+  end
+
+  if plantaro.pest?
+    puts "Oh no! Your plant is infested with bugs. Kill them quick!!" 
+  end
+
+  #options list
+  puts "", "What would you like to do?"
+  puts "-Water\n-Give Sun\n-Sing\n-Spray Pests\n-Status\n-Quit", ""
+  input = gets.chomp
+  if input == "quit"
+    puts "Your plant has lived great life at #{plantaro.age} days old.",
+    "Time to say goodbye :("
+    break
+  end
+
+  case input.downcase
+    when "water"
+      plantaro.water
+    when "give sun"
+      plantaro.give_sun
+    when "sing"
+      plantaro.sing
+    when "spray pests"
+      plantaro.spray
+    when "status"
+      plantaro.check_happiness
+    else
+      puts "Input invalid"
+  end
+  
 end
 
 # puts "Give your little plantling a name?"
@@ -105,8 +199,8 @@ end
 
 
 # puts "plant".red.green
-puts "plant".background(:red)
-puts font.write("plant", letter_spacing: 4)
+# puts "plant".background(:red)
+# puts font.write("plant", letter_spacing: 4)
 
 # Catpix::print_image "Copia de logo_estrechos.jpg",
 #   :limit_x => 1.0,
