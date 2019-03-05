506ba2b0cd77e68d6269aa7a158eabe518df0839
johnr <johnrubio93@gmail.com>
2019-03-05 16:12:23 +1100

    improved code for pests and added more output messages for user action

--- plantaro.rb
+++ plantaro.rb
@@ -3,82 +3,89 @@ require 'date'
 class Plant
   attr_accessor :birthday, :age #just for testing
   
-  @@last_action = []
-  @@messages = {
-    :happy100 => "Your plant is vigorous!", 
-    :happy75 => "Your plant is OK", 
-    :happy50 => "Your plant is feeling standard...",
-    :happy30 => "Your plant is feeling standard...",  
-    :happy20 => "Mate! Your plant is sad and weak!", 
-    :dead => "Your plant died! Rest In Plant... Good plant, better person", 
-    :overwater => "Too much water! This is not the Titanic!", 
-    :oversun => "Too much sun! Your plant became a tomato", 
-    :poison => "Too much pestizide! Achoo!", 
-    :noisy => "Don´t shout!",
-    :infested => "Insects!",
-    :positive => "Good decision!"
-  }
-
   def initialize(username, name, birthday = Date.new(1993, 04, 16))
     @owner = username
     @name = name
-    @happiness = 0
+    @happiness = 20
     @birthday = birthday
     @age = (Date.today - @birthday).to_i
     @pest = false
+    @messages = {
+      :happy100 => "Your plant is vigorous!", 
+      :happy75 => "Your plant is OK", 
+      :happy50 => "Your plant is feeling standard...",
+      :happy30 => "Your plant is feeling standard...",  
+      :happy20 => "Mate! Your plant is sad and weak!",
+      :water => "Your plant is glistening! sparkle sparkle.",
+      :sun => "Your plant is getting a nice healthy tan.",
+      :song => "Your plant is reacting to your angel-like voice.",
+      :healed => "You saved your plant from ferocious pests, Hero!!",
+      :dead => "Your plant died! Rest In Plant... Good plant, better person", 
+      :overwater => "Too much water! This is not the Titanic!", 
+      :oversun => "Too much sun! Your plant became a tomato", 
+      :poison => "Too much pesticide! You plant looks sick!", 
+      :noisy => "Don´t shout!",
+      :infested => "Insects!",
+      :positive => "Good decision!"
+    }
+    @last_action = []
   end
 
   def check_happiness
     if @happiness >= 100
-      puts @@messages[:happy100]
+      puts @messages[:happy100]
     elsif @happiness >= 75
-      puts @@messages[:happy75]
+      puts @messages[:happy75]
     elsif @happiness >= 50
-      puts @@messages[:happy50]
+      puts @messages[:happy50]
     elsif @happiness >= 30
-      puts @@messages[:happy30]
+      puts @messages[:happy30]
     elsif @happiness < 30
-      puts @@messages[:happy20]
+      puts @messages[:happy20]
     end
   end
 
   def water
-    if @@last_action[-1] == @@last_action[-2]
+    if @last_action[-1] == @last_action[-2] && @last_action.size > 1
       @happiness -= 10
-      #then here we must display a sad message
+      puts @messages[:overwater]
     else
       @happiness += 10
-      #then here we must display a good message
+      puts @messages[:water]
     end
-    @@last_action.push "water"
+    @last_action.push "water"
   end
 
   def give_sun
-      if @@last_action[-1] == @@last_action[-2]
+      if @last_action[-1] == @last_action[-2] && @last_action.size > 1
         @happiness -= 10
-        #then here we must display a sad message
+        puts @messages[:oversun]
       else
         @happiness += 10
-        #then here we must display a good message
+        puts @messages[:sun]
       end
-    @@last_action.push "sun"
+    @last_action.push "give_sun"
   end
 
   def sing
     puts "What do you want to sing to your plant?"
     song = gets.chomp
-    if song == song.upcase #in case the owner shouts at the plant
+    #shouting in upcase will reduce happiness
+    if song == song.upcase 
       @happiness -= 10
-      #then here we must display a sad message
+      puts @messages[:noisy]
     else
-      @happiness += 10
-      #then here we must display a good message
+      #length of song will add bonus points to happiness
+      @happiness += 10 + song.length
+      puts @messages[:song]
     end
-    @@last_action.push "sing"
+    @last_action.push "sing"
   end
 
   def pest?
-    if rand(10).even?
+    #33% chance to get infested with bugs, previous action must not be spray
+    #will not run if no other action has been taken first (newly created)
+    if rand(3) == 1 && @last_action.last != "spray" && @last_action != []
       @pest = true
       @happiness -= 20
     end
@@ -87,13 +94,21 @@ class Plant
 
   def spray
     if @pest
-      happiness += 20
+      #spraying plant while infested will return lost points
+      @happiness += 20
+      @pest = false
+      puts @messages[:healed]
     else
       #if plant is not infested with bugs, spray will reduce happiness
-      happiness -= 10
+      @happiness -= 10
+      puts @messages[:poison]
     end
+    @last_action.push "spray"
   end
-  @@last_action.push "spray"
+
+  def death?
+    @happiness <= 0
+  end
 end
 
 
@@ -106,31 +121,36 @@ plantaro = Plant.new "John", plant_name
 
 #program loop
 loop do 
-  #will run pest? method, 50% chance to have bugs
-  puts "Oh no! Your plant is infested with bugs." if plantaro.pest?
-  puts "", "What would you like to do?"
-  puts "-Water\n-Give sun\n-Sing\n-Spray pests\n-Quit", ""
-  input = gets.chomp
+  if plantaro.death?
+    puts "Your plant has withered away dues to poor plant-caring skills :("
+    break
+  end
 
+  if plantaro.pest?
+    puts "Oh no! Your plant is infested with bugs. Kill them quick!!" 
+  end
+
+  #options list
+  puts "", "What would you like to do?"
+  puts "-Water\n-Give sun\n-Sing\n-Spray pests\n-Check status\n-Quit", ""
+  input = gets.chomp
   break if input == "quit"
 
   case input
     when "water"
       plantaro.water
-      puts "You watered plant"
     when "give sun"
       plantaro.give_sun
-      puts "You sat plant in the sun"
     when "sing"
       plantaro.sing
-      puts "You sang to plant"
-    when "spray pests"
-      #plantaro.spray
-      puts "Sprayed pests"
+    when "spray"
+      plantaro.spray
+    when "check"
+      plantaro.check_happiness
     else
       puts "Input invalid"
   end
-
+  
 end
 
 
