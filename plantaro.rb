4888ba0a06e3c77803fe4826b799d15a967365ac
Juanma <juanmasanchezortiz@hotmail.com>
2019-03-05 15:45:04 +1100

    Add table listing options

--- plantaro.rb
+++ plantaro.rb
@@ -1,10 +1,12 @@
 require 'date'
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require 'terminal-table/import'
 
 
 class Plant
   attr_accessor :birthday, :age
   
-messages = {:statuts => ["Your plant is vigorous!", "Your plant is OK", "Your plant is feeling standard...", "Mate! Your plant is sad and weak!", "Your plant died! Rest In Plant... Good plant, better person"], :command => {bad_decidion => ["Too much water! This is not the Titanic!", "Too much sun! Your plant became a tomato", "Too much pestizide! Achoo!", "Don´t shout!", "Insects!"], good decision => "Good decision!"}}
+  # messages = {:statuts => ["Your plant is vigorous!", "Your plant is OK", "Your plant is feeling standard...", "Mate! Your plant is sad and weak!", "Your plant died! Rest In Plant... Good plant, better person"], :command => {bad_decidion => ["Too much water! This is not the Titanic!", "Too much sun! Your plant became a tomato", "Too much pestizide! Achoo!", "Don´t shout!", "Insects!"], good decision => "Good decision!"}}
 #good decision --> messages[2][2]; bad decisions --> messages[2][1][...]; status --> messages[1][...]
   def initialize(username, name, birthday = Date.new(1993, 04, 16))
     @owner = username
@@ -23,6 +25,7 @@ messages = {:statuts => ["Your plant is vigorous!", "Your plant is OK", "Your pl
       #then here we must display a sad message
     else
       @happiness += 10
+      puts @happiness
       #then here we must display a good message
     end
   end
@@ -70,15 +73,27 @@ messages = {:statuts => ["Your plant is vigorous!", "Your plant is OK", "Your pl
 
 end
 
-puts "Give your little plantling a name?"
-name = gets.chomp
-plantaro = Plant.new "John", name
+# puts "Give your little plantling a name?"
+# name = gets.chomp
+# plantaro = Plant.new "John", name
 
-puts plantaro.birthday
-puts plantaro.age
-puts plantaro.sing
-puts plantaro.water_plant
-puts plantaro.give_sun
+# puts plantaro.birthday
+# puts plantaro.age
+# puts plantaro.sing
+# puts plantaro.water_plant
+# puts plantaro.give_sun
 
-#hello
+
+puts
+option_table = table do
+  self.headings = ['Option','Description']
+  add_row ["sun", 'You will give sun to your plant so it can do photosynthesis']
+  add_row :separator
+  add_row ['sing', "You know singing to plants helps give them Carbon Dioxide?"]
+  add_row :separator
+  add_row ['pestizide', "You will kill the insects annoying your plant"]
+  add_row :separator
+  add_row ["water", "You will water your plant so it won´t dehidrate"]
+end
+puts option_table
 
